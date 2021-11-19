75ad1f5d537e19c3d270e9918c253793ab9dc706
insideplant <kami.10pearl@gmail.com>
2021-11-20 01:29:51 +0900

    make box

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +9,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row"
+  },
+  leftContainer: {
+    width: 100
+  },
+  rightContainer: {
+    flex: 1
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+            style={{width: 100, height: 100}}
+            source={{url: "https://picsum.photos/200/300"}}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          </Text>
+          <Text>
+            ReactNews
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
