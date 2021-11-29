133e0f2186c33a7c523c32e20db2d70761d0e854
insideplant <kami.10pearl@gmail.com>
2021-11-30 00:12:38 +0900

    make modal

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers } from "redux";
 import userReducer from './reducers/user'
 import { composeWithDevTools } from "redux-devtools-extension";
 import { persistReducer, persistStore } from "redux-persist";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
